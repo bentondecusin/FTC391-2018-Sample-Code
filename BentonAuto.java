cb64e9a5362ed0f396ff78354f195515c8982531
chemgwoo <2389507122@qq.com>
2018-12-08 01:17:36 -0500

    Update BentonAuto.java

--- BentonAuto.java
+++ BentonAuto.java
@@ -1,4 +1,3 @@
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -12,7 +11,7 @@ public class BentonAuto extends OpMode{
     private DcMotor[] drive = new DcMotor[2];
     private DcMotor dick = null;
     private int counter = 0;
-    
+
     @Override
     public void init() {
         drive[0] = hardwareMap.get(DcMotor.class, "mot0");
@@ -27,54 +26,56 @@ public class BentonAuto extends OpMode{
 
     @Override
     public void init_loop() {
-        
+
     }
 
     @Override
     public void start() {
-        
+
     }
     public ElapsedTime runTime = new ElapsedTime();
 
 
     @Override
     public void loop() {
-if(runTime.time() < 14.2 )
-{
- dick.setPower(-0.75);//lefting mech extending;robot lowering down
-}
-        
-if(runTime.time() > 14.2 && runTime.time() < 15.2){
-        dick.setPower(0);//lifting mech stops
-        drive[1].setPower(5);
-        drive[0].setPower(5);//counter clockwise turning
-}
-        
-if(runTime.time() > 15.2 && runTime.time() < 25.2)
-{
-dick.setPower(0.75);//lifting mech retracts
-drive[1].setPower(0);//stopping moving
-drive[0].setPower(0);//stopping moving
-}
-        
-if(runTime.time() > 17 && runTime.time() < 18){
-
-drive[1].setPower(-5);
-drive[0].setPower(-5);// clockwise turning; direction adjusting 
-}
-        
-if(runTime.time() > 18 && runTime.time() < 23)
-{
-dick.setPower(0.75);//lifting mech retracts
-drive[1].setPower(0);
-drive[0].setPower(0);
-}
-        
-if(runTime.time() > 23){
-  dick.setPower(0);//lifting mech stops
-}
+        if(runTime.time() < 15.2 )
+        {
+            dick.setPower(-0.75);//lefting mech extending;robot lowering down
+        }
+        if(runTime.time() > 15.2 && runTime.time() < 18.2){
+            dick.setPower(0);//lifting mech stops
+        }
+        if(runTime.time() > 18.2 && runTime.time() < 25.2)
+        {
+            dick.setPower(0.75);//lifting mech retracts
+        }
+        if(runTime.time() > 25.2){
+            dick.setPower(0);//lifting mech stops
+        }
 
 
-       
+
+        if(runTime.time() > 15.2 && runTime.time() < 16.2){
+
+            drive[0].setPower(-5);
+            drive[1].setPower(-5);//counter clockwise turning
+        }
+        if(runTime.time() > 16.2 && runTime.time() < 17.2){
+
+            drive[0].setPower(+5);
+            drive[1].setPower(+5);//moveforward
+        }
+        if(runTime.time() > 17.2 && runTime.time() < 23)
+        {
+
+            drive[0].setPower(0);//stopping moving
+
+            drive[1].setPower(0);//stopping moving
+        }
+
+
+
+
+
     }
 }
