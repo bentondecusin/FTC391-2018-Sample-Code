6ccdf4dce8aef06bf3b71e479e663e11b81f4245
Chengyu Benton Li <2389507122@qq.com>
2019-01-08 21:50:46 -0500

    New sample method; Encoders embedded
    New sample method that can determine gold mineral's location by examining at two minerals. Timing-based methods are replaced with encoder-based methods

--- BetaNear.java
+++ BetaNear.java
@@ -9,6 +9,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -33,18 +34,24 @@ public class BetaNear extends LinearOpMode {
     private static final String VUFORIA_KEY = "AUrb6t//////AAABmZ7sUnVME0wvu2pmOKRP5ilgE5gzg4vWVqHNhc0ef2FEwf9NlosWkTS81UmRvZ0UTHFjPeQYLKL6iY60ZJQcJFcMftURUv/1nA/9YELScRwzltxrUAFpfMA/VE9VTaNPTQQYUfm1Z1wUwY6fAJBwDvZJP+UBqPD0AJxz0Gf8jgcdCVgu4A7VtVdk1PRMTSUkHdOEm+VmXzpjxL9X4d/v81mx3aqJbVc6+qhUD53umiep/wCgl9WxHYY6ZEM2tuS7Eih3TexL24HLFvdEu79t24yTzCFz6du/hB12nfyySO78UWbdlusHuHIv0ZI5/IWh4RigF057FaLWc4F+EluGBkO0c6ygIaciN5fHPS9l7dtj";
     String goldLocation;
 
-    //configure motors
 
+    //set up encoders
+    static final double COUNTS_Per_REV    = 1140 ;
+    static final double WHEEL_DIAMETER = 4 ; //in inches
+    static final double COUNTS_Per_INCH = COUNTS_Per_REV/(WHEEL_DIAMETER*Math.PI);
+    static final double COUNTS_Per_DEGREE = (COUNTS_Per_REV/(WHEEL_DIAMETER/18))/360;
+    //configure motors
     private DcMotor left = null;
     private DcMotor right = null;
     private DcMotor lift = null;
     private Servo launch = null;
 
-
+    //st up time
     ElapsedTime runTime = new ElapsedTime();
     double checkpoint1 = 10;
 
-
+    //set speed
+    static final double speed = .5 ;
 
     public void sample() {
         runTime.reset();
@@ -60,7 +67,7 @@ public class BetaNear extends LinearOpMode {
                 List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
                 if (updatedRecognitions != null) {
                     telemetry.addData("# Object Detected", updatedRecognitions.size());
-                    if (updatedRecognitions.size() == 3) {
+                    if (updatedRecognitions.size() == 2) {
                         int goldMineralX = -1;
                         int silverMineral1X = -1;
                         int silverMineral2X = -1;
@@ -73,32 +80,37 @@ public class BetaNear extends LinearOpMode {
                                 silverMineral2X = (int) recognition.getLeft();
                             }
                         }
-                        if (goldMineralX != -1 && silverMineral1X != -1 && silverMineral2X != -1) {
-                            if (goldMineralX < silverMineral1X && goldMineralX < silverMineral2X) {
+                        if (silverMineral1X != -1 && silverMineral2X != -1) {
+                            telemetry.addData("Gold Mineral Position", "Left");
+                            goldLocation = "L";
+                            break;
+                        }
+                        if (silverMineral1X != -1 && goldMineralX != -1) {
+                            if (goldMineralX < silverMineral1X) {
                                 telemetry.addData("Gold Mineral Position", "Left");
-                                goldLocation = "L";
-                                break;
-
-                            } else if (goldMineralX > silverMineral1X && goldMineralX > silverMineral2X) {
-                                telemetry.addData("Gold Mineral Position", "Right");
-                                goldLocation = "R";
-                                break;
-
-                            } else {
-                                telemetry.addData("Gold Mineral Position", "Center");
                                 goldLocation = "C";
                                 break;
                             }
                         }
+                        if (silverMineral1X != -1 && goldMineralX != -1) {
+                            if (goldMineralX > silverMineral1X) {
+                                telemetry.addData("Gold Mineral Position", "Left");
+                                goldLocation = "R";
+                                break;
+                            }
+                        }
+
+
                     }
-                    telemetry.update();
                 }
+                telemetry.update();
             }
         }
         //Four endings for this story: left, right, center, nothing
-
-
     }
+
+
+
 
     private void initVuforia() {
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
@@ -124,37 +136,92 @@ public class BetaNear extends LinearOpMode {
          launch = hardwareMap.get(Servo.class,"ser");
         //set rotational direction
 
-         left.setDirection(DcMotor.Direction.FORWARD);
-         right.setDirection(DcMotor.Direction.REVERSE);
+         left.setDirection(DcMotor.Direction.REVERSE);
+         right.setDirection(DcMotor.Direction.FORWARD);
          lift.setDirection(DcMotor.Direction.FORWARD);
 
 
         waitForStart();
     }
 
-    public void linearMove(double dX){//dx means displacement
+
+    public void moveForward(double dX){//dx means displacement in inches
         runTime.reset();
-        left.setPower(.5);//power depends on the the robot and case studies are needed
-        right.setPower(.5);
-        while (runTime.seconds()<dX){
-            telemetry.addData("time",runTime.seconds());
-            telemetry.update();
-        }
-        left.setPower(0);
-        right.setPower(0);
-    }
-    public void angularMove(double dA){//dA means change in angular degree
-        runTime.reset();;
-        left.setPower(.5);
-        right.setPower(-.5);//power depends on the the robot and case studies are needed
-        while (runTime.seconds()<dA){
-            telemetry.addData("time",runTime.seconds());
+        left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        left.setTargetPosition((int)(dX * COUNTS_Per_INCH));
+        right.setTargetPosition((int)(dX * COUNTS_Per_INCH));
+
+        left.setPower(speed);
+        right.setPower(speed);
+        while (left.isBusy()){
+            telemetry.addData("Left",left.getCurrentPosition());
+            telemetry.addData("Right",right.getCurrentPosition());
             telemetry.update();
         }
         left.setPower(0);
         right.setPower(0);
     }
 
+    public void moveBackward(double dX){//dx means displacement in inches
+        runTime.reset();
+        left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        left.setTargetPosition((int)(dX * COUNTS_Per_INCH));
+        right.setTargetPosition((int)(dX * COUNTS_Per_INCH));
+
+        left.setPower(-speed);
+        right.setPower(-speed);
+        while (left.isBusy()){
+            telemetry.addData("Left",left.getCurrentPosition());
+            telemetry.addData("Right",right.getCurrentPosition());
+            telemetry.update();
+        }
+        left.setPower(0);
+        right.setPower(0);
+    }
+    public void turnClockwise(double dA){//dx means displacement
+        runTime.reset();
+        left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        left.setTargetPosition((int)(dA * COUNTS_Per_DEGREE));
+        right.setTargetPosition((int)(-dA * COUNTS_Per_DEGREE));
+
+        left.setPower(.5);//power depends on the the robot and case studies are needed
+        right.setPower(-.5);
+        while (left.isBusy()){
+            telemetry.addData("Left",left.getCurrentPosition());
+            telemetry.addData("Right",right.getCurrentPosition());
+            telemetry.update();
+        }
+        left.setPower(0);
+        right.setPower(0);
+    }
+    public void turnCounterClockwise(double dA){//dx means displacement
+        runTime.reset();
+        left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        left.setTargetPosition((int)(-dA * COUNTS_Per_DEGREE));
+        right.setTargetPosition((int)(dA * COUNTS_Per_DEGREE));
+
+        left.setPower(-.5);//power depends on the the robot and case studies are needed
+        right.setPower(.5);
+        while (left.isBusy()){
+            telemetry.addData("Left",left.getCurrentPosition());
+            telemetry.addData("Right",right.getCurrentPosition());
+            telemetry.update();
+        }
+        left.setPower(0);
+        right.setPower(0);
+    }
     private void pitch() {//pitch the ball means game starts. lower down, leave the latch, come up right in front of the mineral
 
     }
@@ -162,27 +229,27 @@ public class BetaNear extends LinearOpMode {
     private void bat(String location) {
 
         if (location == "L") {
-            linearMove(5);
-            angularMove(-45);
-            linearMove(15);
-            angularMove(90);
-            linearMove(10);
+
+            turnCounterClockwise(45);
+            moveForward(35);
+            turnClockwise(90);
+            moveForward(34);
         }
         if (location == "R") {
-            linearMove(5);
-            angularMove(45);
-            linearMove(15);
-            angularMove(-90);
-            linearMove(10);
+
+            turnClockwise(45);
+            moveForward(35);
+            turnCounterClockwise(90);
+            moveForward(34);
         }
         if (location == "C" ||location == "N") {
-            linearMove(20);
+            moveForward(38);
 
         }
     }
 
     private void homeRun() {
-        launch.setPosition(20);
+        launch.setPosition(220);
     }
 
 
@@ -196,6 +263,7 @@ public class BetaNear extends LinearOpMode {
         }
 
     }
+
 
 
 
